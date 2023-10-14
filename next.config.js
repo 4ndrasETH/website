b12e5d1470db74d3e8636178c6318c11214f9aa3
Andras Szucs <andras@andras.codes>
2023-10-14 16:03:42 +0200

    feat: add redirects for ens and lens

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const redirects = [
       "https://app.icebreaker.xyz/eth/0xff001e305dCc787Da1D1ff554C25f8876A37cb22?key=UGqhZyU993wNk_ZbU7Zve",
     permanent: true,
   },
+  {
+    source: "/ens",
+    destination: "https://app.ens.domains/andrascodes.eth",
+    permanent: true,
+  },
+  {
+    source: "/lens",
+    destination: "https://www.lensfrens.xyz/4ndras.lens",
+    permanent: true,
+  },
 ];
 
 const nextConfig = {
