9d2ddb3cc08f73a8e6d2e6c549e4ea2e2f347965
Andras Szucs <andras@andras.codes>
2023-10-14 14:56:37 +0200

    feat: add redirect for icebreaker

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const redirects = [
+  {
+    source: "/icebreaker",
+    destination:
+      "https://app.icebreaker.xyz/eth/0xff001e305dCc787Da1D1ff554C25f8876A37cb22?key=UGqhZyU993wNk_ZbU7Zve",
+    permanent: true,
+  },
+];
+
+const nextConfig = {
+  async redirects() {
+    return redirects;
+  },
+};
+
+module.exports = nextConfig;
